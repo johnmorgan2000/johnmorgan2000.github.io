6bfa5840e01475d85cc4bca2819a0490ab8eadd0
John <jmorgan@basecampcodingacademy.org>
2019-04-30 14:12:49 -0500

    Fixed display issues happening on other systems.

--- DATA.js
+++ DATA.js
@@ -1,16 +1,13 @@
 const DATA = {
-    bio: `My name is John Morgan and I would like to be hired as a software developer. I am currently
-    a student at Base Camp Coding Academy in Watervalley, Ms. Here at BCCA, we work hard 8 hours a day, 5 
-    days a week for 11 months. I will be graduating and ready to be hired towards the end of May going into
-    June. When it comes to writing code,I have no preference between the frontend or the backend. If you have any
+    bio: `My name is John Morgan and am looking to start my career as a software developer. I am currently
+    a student at Base Camp Coding Academy in Water Valley, MS. Here at BCCA, we work hard 8 hours a day, 5 
+    days a week for 11 months doing hands-on programming to build our skills. I am comfortable with a wide range of full-stack web technologies and you can see some of my work below. If you have any
     project requests or questions, feel free to contact me anytime with details.`,
-
 
     projects: [
         {
             title: "Emoji Maker",
-            github:
-                "https://github.com/johnmorgan2000/Emoji-Maker",
+            github: "https://github.com/johnmorgan2000/Emoji-Maker",
             liveDemo: "https://emoji-maker.netlify.com/",
             description:
                 "Learned how to use React.js to make a simple emoji maker during independent study time."
@@ -19,7 +16,8 @@ const DATA = {
             title: "Ping-o Pong-o",
             github:
                 "https://github.com/johnmorgan2000/johnmorgan-web-rental-agency",
-            liveDemo: "https://johnmorgan2000.github.io/johnmorgan-web-rental-agency/?payment=on",
+            liveDemo:
+                "https://johnmorgan2000.github.io/johnmorgan-web-rental-agency/?payment=on",
             description:
                 "Developed a simulation of a rental site for ping pong supplies."
         },
